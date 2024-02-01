3ed74536d41cfd56bbb8dc119df246b43767ef46
Roman Viktorov <rpviktorov@gmail.com>
2024-02-01 23:25:14 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Hare {
+
+    void tryEat(Ball ball) {
+        ball.tryRun(false);
+    }
+
+}
---
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Wolf {
+
+    void tryEat(Ball ball) {
+        ball.tryRun(false);
+    }
+}
