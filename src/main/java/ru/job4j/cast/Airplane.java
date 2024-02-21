733fd02ccd0ce07f3e50fa5442a30aef6ef6f852
Roman Viktorov <rpviktorov@gmail.com>
2024-02-21 14:43:42 +0300

    fix: 5.1. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/cast/Airplane.java
+++ src/main/java/ru/job4j/cast/Airplane.java
@@ -1,6 +1,6 @@
 package ru.job4j.cast;
 
-public class Airplane implements Vehicle{
+public class Airplane implements Vehicle {
 
     @Override
     public void move() {
--- src/main/java/ru/job4j/cast/Train.java
+++ src/main/java/ru/job4j/cast/Train.java
@@ -5,4 +5,5 @@ public class Train implements Vehicle {
     public void move() {
         System.out.println("Поезд едет по рельсам");
     }
+
 }
