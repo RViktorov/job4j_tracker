16a8f163912b9b2ad1eae4355be28a4da036e519
Roman Viktorov <rpviktorov@gmail.com>
2024-02-26 23:24:30 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User result = null;
+        for (User user : users)
+            if (user.getUsername().equals(login)) {
+                result = user;
+            } else {
+                throw new UserNotFoundException("user is not found");
+            }
+        return result;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
+            throw new UserInvalidException(("user is not valid"));
+        }
+        return user.isValid();
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+
+        } catch (UserInvalidException ea) {
+            ea.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
