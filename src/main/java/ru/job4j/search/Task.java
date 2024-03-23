e4c61ed5c544c346a1495f7ee346f81b0f45720e
Roman Viktorov <rpviktorov@gmail.com>
2024-03-23 23:00:18 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+
+public class Task {
+    private String description;
+    private int priority;
+
+    public Task(String description, int priority) {
+        this.description = description;
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
