4632771caf2221c3c8ccb110335ded90f1179f51
Roman Viktorov <rpviktorov@gmail.com>
2024-03-30 22:46:25 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> result = new HashMap<>();
+        result.put("ivanov@rambler.ru", "Ivanov Ivan Ivanovich");
+        result.put("petrov@rambler.ru", "Petrov Petr Ivanovich");
+        result.put("sidorov@rambler.ru", "Sidorov Petr Ivanovich");
+        for (String key : result.keySet()) {
+            System.out.println(result.get(key));
+        }
+    }
+}
