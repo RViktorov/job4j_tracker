8ee69fc858242d1054923a0a377101a042479bdd
Roman Viktorov <rpviktorov@gmail.com>
2024-04-06 12:44:57 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemAscByNameTest {
+    @Test
+    void whenItemAscByName() {
+        List<Item> lists = new ArrayList<>();
+        lists.add(new Item(1, "Sleep"));
+        lists.add(new Item(2, "Run"));
+        lists.add(new Item(3, "Go"));
+        lists.sort(new ItemAscByName());
+        List<Item> items = new ArrayList<>(lists);
+        List<Item> expected = new ArrayList<>(List.of(new Item(3, "Go"), new Item(2, "Run"), new Item(1, "Sleep")));
+        assertThat(items).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.ExitAction;
+import ru.job4j.tracker.action.UserAction;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.MockInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemDescByNameTest {
+
+    @Test
+    void whenItemDescByName() {
+        List<Item> lists = new ArrayList<>();
+        lists.add(new Item(1, "Sleep"));
+        lists.add(new Item(2, "Run"));
+        lists.add(new Item(3, "Go"));
+        lists.sort(new ItemDescByName());
+        List<Item> items = new ArrayList<>(lists);
+        List<Item> expected = new ArrayList<>(List.of(new Item(1, "Sleep"), new Item(2, "Run"), new Item(3, "Go")));
+        assertThat(items).isEqualTo(expected);
+    }
+}
