a2c3ce48d7c763ce6b448ae513033ce76f0c6363
Roman Viktorov <rpviktorov@gmail.com>
2024-04-08 15:22:30 +0300

    fix: 3. Компаратор для строк [#35008 #490294]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,35 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = 0;
-        if (o1.length() > o2.length()) {
-            for (int i = 0; i < o2.length(); i++) {
-                result = Character.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    break;
-                } else {
-                    result = o1.length() - o2.length();
-                }
+        for (int i = 0; i < Math.min(o1.length(), o2.length()); i++) {
+            if (o1.charAt(i) != o2.charAt(i)) {
+                return Character.compare(o1.charAt(i), o2.charAt(i));
             }
         }
-        if (o1.length() < o2.length()) {
-            for (int i = 0; i < o1.length(); i++) {
-                result = Character.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    break;
-                } else {
-                    result = o1.length() - o2.length();
-                }
-            }
-        }
-        if (o1.length() == o2.length()) {
-            for (int i = 0; i < o1.length(); i++) {
-                result = Character.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    break;
-                }
-            }
-        }
-        return result;
+        return Integer.compare(o1.length(), o2.length());
     }
 }
