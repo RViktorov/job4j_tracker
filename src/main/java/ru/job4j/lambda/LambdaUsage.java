b87d740f04a81fb52efe79a540185539ecf81035
Roman Viktorov <rpviktorov@gmail.com>
2024-04-15 15:14:17 +0300

    fix: 2.1. Лямбда блок [#249208 #491260

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -8,10 +8,19 @@ public class LambdaUsage {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
-            System.out.println("compare - " + right.length() + " : " + left.length());
+            System.out.println("compare - " + left.length() + " : " + right.length());
             return Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        Comparator<String> comparatorDescSize = (left, right) -> {
+            return Integer.compare(right.length(), left.length());
+        };
+
+        strings.sort(comparatorDescSize);
         for (String string : strings) {
             System.out.println(string);
         }
