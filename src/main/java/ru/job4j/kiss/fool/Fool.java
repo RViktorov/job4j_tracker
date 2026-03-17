a8c62e59b21767e80a61e9c449af0ca2db4894e3
Roman Viktorov <rpviktorov@gmail.com>
2026-03-17 22:11:59 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -0,0 +1,40 @@
+package ru.job4j.kiss.fool;
+
+import java.util.Scanner;
+
+public class Fool {
+    public static void main(String[] args) {
+        System.out.println("Игра FizzBuzz.");
+        int startAt = 1;
+        Scanner input = new Scanner(System.in);
+        while (startAt < 100) {
+
+            String computerAnswer = getFizzBuzz(startAt);
+            System.out.println(computerAnswer);
+            startAt++;
+            String correctAnswer = getFizzBuzz(startAt);
+            String userAnswer = input.nextLine().trim();
+
+            if (!correctAnswer.equalsIgnoreCase(userAnswer)) {
+                System.out.println("Ошибка. Начинай снова.");
+                startAt = 1;
+                continue;
+            }
+            startAt++;
+        }
+    }
+
+    private static String getFizzBuzz(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
+}
