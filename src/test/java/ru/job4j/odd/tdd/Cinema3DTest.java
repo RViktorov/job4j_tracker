c14c2b5ee432ae7e9bf4e5456e6e3d0632ffc140
Roman Viktorov <rpviktorov@gmail.com>
2026-03-18 20:06:22 +0300

    3. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/odd/tdd/Cinema3DTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.odd.tdd;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.tdd.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
+public class Cinema3DTest {
+    @Test
+    public void whenBuyThenGetTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenAddSessionThenItExistsBetweenAllSessions() {
+        Cinema cinema = new Cinema3D();
+        Session session = new Session3D();
+        cinema.add(session);
+        List<Session> sessions = cinema.find(data -> true);
+        assertThat(sessions).contains(session);
+    }
+
+    @Test
+    public void whenBuyOnInvalidRowThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, -1, 1, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenAddSeveralSessionsThenAllStored() {//Проверяем что сохраняется несколько сеансов
+        Cinema cinema = new Cinema3D();
+        Session s1 = new Session3D();
+        Session s2 = new Session3D();
+        cinema.add(s1);
+        cinema.add(s2);
+        List<Session> result = cinema.find(s -> true);
+
+        assertThat(result).containsExactlyInAnyOrder(s1, s2);
+    }
+
+    @Test
+    public void whenBuySameSeatTwiceThenException() { //Повторная покупка одного и того же места
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        cinema.buy(account, 1, 1, date);
+
+        assertThatThrownBy(() -> cinema.buy(account, 1, 1, date))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
